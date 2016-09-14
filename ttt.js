fb1b80b590df6c5495e51e69fa90543fa682bad1
Victor McLean <vmvictorvm@gmail.com>
2016-09-14 18:56:24 -0400

    refactor code

--- ttt.js
+++ ttt.js
@@ -5,27 +5,7 @@ $(function () {
   var turn = 0;
   var win = false;
 
-  $('.1').on('click', function(){
-    //alert("Squre 1 was clicked");
-    var self = $(this);
-    writeText(self);
-    checkResult();
-    if (isFull() && !(win)) {
-      $('#result').html('NO WINNER');
-    }
-  });
-
-  $('.2').on('click', function(){
-    //alert("Squre 1 was clicked");
-    var self = $(this);
-    writeText(self);
-    checkResult();
-    if (isFull() && !(win)) {
-      $('#result').html('NO WINNER');
-    }
-  });
-
-  $('.3').on('click', function(){
+  $('.square').on('click', function(){
     //alert("Squre 1 was clicked");
     var self = $(this);
     writeText(self);
@@ -36,113 +16,54 @@ $(function () {
   });
 
 
-  $('.4').on('click', function(){
-    //alert("Squre 1 was clicked");
-    var self = $(this);
-    writeText(self);
-    checkResult();
-    if (isFull() && !(win)) {
-      $('#result').html('NO WINNER');
-    }
-  });
-
-  $('.5').on('click', function(){
-    //alert("Squre 1 was clicked");
-    var self = $(this);
-    writeText(self);
-    checkResult();
-    if (isFull() && !(win)) {
-      $('#result').html('NO WINNER');
-    }
-  });
-
-  $('.6').on('click', function(){
-    //alert("Squre 1 was clicked");
-    var self = $(this);
-    writeText(self);
-    checkResult();
-    if (isFull() && !(win)) {
-      $('#result').html('NO WINNER');
-    }
-  });
-
-  $('.7').on('click', function(){
-    //alert("Squre 1 was clicked");
-    var self = $(this);
-    writeText(self);
-    checkResult();
-    if (isFull() && !(win)) {
-      $('#result').html('NO WINNER');
-    }
-  });
-
-  $('.8').on('click', function(){
-    //alert("Squre 1 was clicked");
-    var self = $(this);
-    writeText(self);
-    checkResult();
-    if (isFull() && !(win)) {
-      $('#result').html('NO WINNER');
-    }
-  });
-
-  $('.9').on('click', function(){
-    //alert("Squre 1 was clicked");
-    var self = $(this);
-    writeText(self);
-    checkResult();
-    if (isFull() && !(win)) {
-      $('#result').html('NO WINNER');
-    }
-  });
 
   function checkResult() {
-    if (($('.1').text() === $('.2').text()) && ($('.1').text() === $('.3').text())) {
-      if ($('.1').text() !== '') {
-        $('#result').html('The winner team is: ' + $('.1').text());
+    if (($('#1').text() === $('#2').text()) && ($('#1').text() === $('#3').text())) {
+      if ($('#1').text() !== '') {
+        $('#result').html('The winner team is: ' + $('#1').text());
         win = true;
       }
     }
-    if (($('.4').text() === $('.5').text()) && ($('.4').text() === $('.6').text())) {
-      if ($('.4').text() !== '') {
-      $('#result').html('The winner team is: ' + $('.4').text());
+    if (($('#4').text() === $('#5').text()) && ($('#4').text() === $('#6').text())) {
+      if ($('#4').text() !== '') {
+      $('#result').html('The winner team is: ' + $('#4').text());
       win = true;
     }
     }
-    if (($('.7').text() === $('.8').text()) && ($('.7').text() === $('.9').text())) {
-      if ($('.7').text() !== '') {
-      $('#result').html('The winner team is: ' + $('.7').text());
+    if (($('#7').text() === $('#8').text()) && ($('#7').text() === $('#9').text())) {
+      if ($('#7').text() !== '') {
+      $('#result').html('The winner team is: ' + $('#7').text());
       win = true;
     }
     }
-    if (($('.1').text() === $('.4').text()) && ($('.1').text() === $('.7').text())) {
-      if ($('.1').text() !== '') {
-      $('#result').html('The winner team is: ' + $('.1').text());
+    if (($('#1').text() === $('#4').text()) && ($('#1').text() === $('#7').text())) {
+      if ($('#1').text() !== '') {
+      $('#result').html('The winner team is: ' + $('#1').text());
       win = true;
     }
     }
 
-    if (($('.2').text() === $('.5').text()) && ($('.2').text() === $('.8').text())) {
-      if ($('.2').text() !== '') {
-      $('#result').html('The winner team is: ' + $('.2').text());
+    if (($('#2').text() === $('#5').text()) && ($('#2').text() === $('#8').text())) {
+      if ($('#2').text() !== '') {
+      $('#result').html('The winner team is: ' + $('#2').text());
       win = true;
     }
     }
-    if (($('.3').text() === $('.6').text()) && ($('.3').text() === $('.9').text())) {
-      if ($('.3').text() !== '') {
-      $('#result').html('The winner team is: ' + $('.3').text());
+    if (($('#3').text() === $('#6').text()) && ($('#3').text() === $('#9').text())) {
+      if ($('#3').text() !== '') {
+      $('#result').html('The winner team is: ' + $('#3').text());
       win = true;
     }
     }
-    if (($('.1').text() === $('.5').text()) && ($('.1').text() === $('.9').text())) {
-      if ($('.1').text() !== '') {
-      $('#result').html('The winner team is: ' + $('.1').text());
+    if (($('#1').text() === $('#5').text()) && ($('#1').text() === $('#9').text())) {
+      if ($('#1').text() !== '') {
+      $('#result').html('The winner team is: ' + $('#1').text());
       win = true;
     }
     }
-    if (($('.3').text() === $('.5').text()) && ($('.3').text() === $('.7').text())) {
-      if ($('.3').text() !== '') {
-      $('#result').html('The winner team is: ' + $('.3').text());
+    if (($('#3').text() === $('#5').text()) && ($('#3').text() === $('#7').text())) {
+      if ($('#3').text() !== '') {
+      $('#result').html('The winner team is: ' + $('#3').text());
       win = true;
     }
     }
