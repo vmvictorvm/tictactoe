33aaef5bb36a279b7214e7d4136ec58949910e6f
Victor McLean <vmvictorvm@gmail.com>
2016-09-14 21:21:17 -0400

    update css

--- ttt.js
+++ ttt.js
@@ -20,66 +20,66 @@ $(function () {
 
   function checkResult() {
     if ($('#1').text() !== '' && $('#1').text() === $('#2').text() && $('#1').text() === $('#3').text()) {
-      $('#result').html('The winner team is: ' + $('#1').text());
+      $('#result').html('The winner is: ' + $('#1').text());
       win = true;
-      $('button').css('display', 'inline-block');  //Show refresh button once the match ended
+      $('button').css('display', 'block');  //Show refresh button once the match ended
       $('.square').css({'pointer-events':'none',   //Grey-out and disabled boar once the match ended
                     'background-color':'grey'});
     }
     else if ($('#4').text() !== '' && $('#4').text() === $('#5').text() && $('#4').text() === $('#6').text()) {
-      $('#result').html('The winner team is: ' + $('#4').text());
+      $('#result').html('The winner is: ' + $('#4').text());
       win = true;
-      $('button').css('display', 'inline-block');
+      $('button').css('display', 'block');
       $('.square').css({'pointer-events':'none',
                     'background-color':'grey'});
     }
     else if ($('#7').text() !== '' && $('#7').text() === $('#8').text() && $('#7').text() === $('#9').text()) {
-      $('#result').html('The winner team is: ' + $('#7').text());
+      $('#result').html('The winner is: ' + $('#7').text());
       win = true;
-      $('button').css('display', 'inline-block');
+      $('button').css('display', 'block');
       $('.square').css({'pointer-events':'none',
                     'background-color':'grey'});
     }
     else if ($('#1').text() !== '' && $('#1').text() === $('#4').text() && $('#1').text() === $('#7').text()) {
-      $('#result').html('The winner team is: ' + $('#1').text());
+      $('#result').html('The winner is: ' + $('#1').text());
       win = true;
-      $('button').css('display', 'inline-block');
+      $('button').css('display', 'block');
       $('.square').css({'pointer-events':'none',
                     'background-color':'grey'});
     }
 
     else if ($('#2').text() !== '' && $('#2').text() === $('#5').text() && $('#2').text() === $('#8').text()) {
-      $('#result').html('The winner team is: ' + $('#2').text());
+      $('#result').html('The winner is: ' + $('#2').text());
       win = true;
-      $('button').css('display', 'inline-block');
+      $('button').css('display', 'block');
       $('.square').css({'pointer-events':'none',
                     'background-color':'grey'});
 
     }
     else if ($('#3').text() !== '' && $('#3').text() === $('#6').text() && $('#3').text() === $('#9').text()) {
-      $('#result').html('The winner team is: ' + $('#3').text());
+      $('#result').html('The winner is: ' + $('#3').text());
       win = true;
-      $('button').css('display', 'inline-block');
+      $('button').css('display', 'block');
       $('.square').css({'pointer-events':'none',
                     'background-color':'grey'});
     }
     else if ($('#1').text() !== '' && $('#1').text() === $('#5').text() && $('#1').text() === $('#9').text()) {
-      $('#result').html('The winner team is: ' + $('#1').text());
+      $('#result').html('The winner is: ' + $('#1').text());
       win = true;
-      $('button').css('display', 'inline-block');
+      $('button').css('display', 'block');
       $('.square').css({'pointer-events':'none',
                     'background-color':'grey'});
     }
     else if ($('#3').text() !== '' && $('#3').text() === $('#5').text() && $('#3').text() === $('#7').text()) {
-      $('#result').html('The winner team is: ' + $('#3').text());
+      $('#result').html('The winner is: ' + $('#3').text());
       win = true;
-      $('button').css('display', 'inline-block');
+      $('button').css('display', 'block');
       $('.square').css({'pointer-events':'none',
                     'background-color':'grey'});
     }
     else if (isFull() && !(win)) {
       $('#result').html('NO WINNER');
-      $('button').css('display', 'inline-block');
+      $('button').css('display', 'block');
       $('.square').css({'pointer-events':'none',
                     'background-color':'grey'});
     }
